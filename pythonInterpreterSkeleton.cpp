318829a363a99fc51d8b3647580f3122676ea2b5
crazyscarecrow <42651599+crazyscarecrow@users.noreply.github.com>
2019-10-29 10:21:15 -0600

    updated classes / added comments

--- pythonInterpreterSkeleton.cpp
+++ pythonInterpreterSkeleton.cpp
@@ -8,19 +8,8 @@
 using namespace std;
 
 typedef enum { /*Types of statement*/ INVALID } StatementType;
-typedef enum { /*Types of terminal*/ EOL, BAD_TOKEN } TokenType; 
+typedef enum { /*Types of terminal*/ IDENTIFIER, EOL, BAD_TOKEN } TokenType; 
 typedef enum { /*valid dataTypes*/ NONE } DataType; 
-
-class Statement{
-    StatementType type;
-    string value;
-public:
-    Statement(StatementType newType = INVALID, string newValue = ""){
-        type = newType;
-        value = newValue;
-    }
-    StatementType getType(){ return type; }
-};
 
 class Token{
     TokenType type;
@@ -29,6 +18,44 @@ public:
     Token(TokenType newType = EOL, string newValue = ""){
         type = newType;
         value = newValue;
+    }
+    TokenType getType(){ return type; }
+    string getValue(){ return value; }
+};
+
+class Statement{
+    StatementType type;
+    vector<Token> tokens;
+    int pos;
+public:
+    Statement(StatementType newType = INVALID, vector<Token> newTokens = new vector<Token>, ){
+        type = newType;
+        value = newTokens;
+        pos = 0;
+    }
+
+    StatementType getType(){ return type; }
+    void setPos(int newPos){ pos = newPos; }
+
+    Token peek(){
+
+        // Return next token without iterating pos
+
+        return Token();
+    }
+
+    Token getToken(){
+
+        // Return next token and iterate pos
+
+        return Token();
+    }
+
+    Token lastToken(){
+
+        // Decrement pos then return current token
+
+        return Token();
     }
 };
 
@@ -45,12 +72,16 @@ class Tokenizer{
 	}
 
     Token peek(){
+
         // Return next token without iterating pos
+
         return Token();
     }
 
     Token getToken(){
+
         // Return next token and iterate pos
+
         return Token();
     }
 };
@@ -63,14 +94,22 @@ class Parser{
 public:
 
     // Functions for terminals
+    //
+    // The Parser doesn't care if a Variable has been declared,
+    // it only cares if a valid IDENTIFIER is used in the right place
+    //
+    // If an IDENTIFIER is used before being declared as a Variable
+    // the Interpreter should handle it a runtime error
 
     Statement parse(string input){
 
         // Re-initialize the parser
         // and run input through it
         // 
-        // Output an error if there's a problem
-        // return a Statement
+        // Output an error and return an INVALID Statement
+        // if there's a problem
+        //
+        // Otherwise, return the parsed Statement
 
         return Statement();
     }
@@ -84,19 +123,28 @@ public:
         type = newType;
         value = newValue;
     }
+    DataType getType(){ return type; }
 };
 
 class Interpreter{
     // string = identifier, Variable = value
     map<string, Variable> variables;
+    vector<Statement> program;
+    string error;
+    // Increment/Decrement line for loops and conditional statements
+    int line;
+    // Used to keep track of loop/conditional level
+    int tabDepth;
 public:
 
     // Functions for statement types (e.g. print, assignment, etc.)
 
-    void interpret(Statement input){
+    void interpret(vector<Statement> input){
 
         // Run input through the appropriate function
         // Do nothing if input is INVALID
+        //
+        // Output an error and stop running if there's a problem
 
     } 
 };
@@ -105,14 +153,16 @@ int main(){
     Parser parser;
     Interpreter interpreter;
     vector<string> input;
+    vector<Statement> program;
+    bool error = false;
 
-    // read in input
+    // Read input
 
-    for( string s : input ){
-        Statement next = parser.parse(s);
-        interpreter.interpret(next);
-        if(next.getType() == INVALID) break;
-    }
+    for(int i=0; i < input.size() && !error; i++)
+        program.push_back(parser.parse(input[i]));
+
+    if(!error)
+        interpreter.interpret(program);
 
     return 0;
 }
